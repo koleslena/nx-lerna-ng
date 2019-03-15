bc0bacee35bb183d99941f113405c4969d923641
Elena Kolesnikova <koleslena@yandex.ru>
2019-03-15 23:44:19 +0600

    second

--- apps/nx-lerna-ng/src/app/app.module.ts
+++ apps/nx-lerna-ng/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { TestComponentComponent } from './test-component/test-component.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TestComponentComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
